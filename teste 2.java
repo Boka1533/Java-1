efcf03cfaad99b6f8996af3b80507dbc59f6cbfc
Boka1533 <162579970+Boka1533@users.noreply.github.com>
2024-03-07 09:23:55 -0300

    Add files via upload

---
+++ teste 2.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	   float n1 = 6;
+	   float n2 = 7;
+	   float n3 = 7;
+	   float n4 = 9;
+	   
+	   float nota = (n1 + n2 + n3 + n4)/4;
+	   
+	   if (nota >= 7){
+	       System.out.println("Aprovado");
+	       
+	   }
+	else{
+	    System.out.println("Reprovado");
+	}
+		
+		
+	}
+}
