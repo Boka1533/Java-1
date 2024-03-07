cd83ddcd81e88f65cbeb5adb5c959b9a4748a23b
Boka1533 <162579970+Boka1533@users.noreply.github.com>
2024-03-07 09:01:15 -0300

    Soma de numeros (java)

---
+++ Main.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    float x = -2;
+	    
+	    float expressao = 2*x+3*x-4*x;
+	    System.out.println("O resultado é:"+ expressao);
+	    
+       float expressao_2 = (3*x)/(4*x)+5*x-7*x;
+	   System.out.println("O resultado é:"+ expressao_2); 
+	   
+	    float expressao_3 = 5*x+6*x-x+10*x;
+	    System.out.println("O resultado é:"+ expressao_3);
+	    
+	    
+		
+		
+		
+	}
+}
